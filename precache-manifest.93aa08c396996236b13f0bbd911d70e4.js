b013a47541b8cfacabdf06017d30e1f84fc65bdb
Michał Pobuta <michal.pobuta@beautyofscience.pl>
2023-12-11 15:40:02 +0100

    Updates

---
+++ precache-manifest.93aa08c396996236b13f0bbd911d70e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d90175f67dd22bbbe74fb94d0fd76a0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "731b9dced8b99845d894",
+    "url": "/react-app/static/css/main.90de9b9c.chunk.css"
+  },
+  {
+    "revision": "b3d8d430703aead067ec",
+    "url": "/react-app/static/js/2.df8969be.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/react-app/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "731b9dced8b99845d894",
+    "url": "/react-app/static/js/main.2085c6ab.chunk.js"
+  },
+  {
+    "revision": "284af93356e22e186bac",
+    "url": "/react-app/static/js/runtime~main.35393859.js"
+  }
+]);
